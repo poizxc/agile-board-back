904e4c0bdae75946405158d954897a60a4caee13
poizxc <poizxc@users.noreply.github.com>
2020-07-26 19:17:01 +0200

    added some basic security

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const helmet = require('helmet');
 
 const reqID = require('./middlewares/reqID');
 const requestLogger = require('./middlewares/requestLogger');
@@ -11,10 +12,12 @@ const controllers = require('./controllers');
 const logger = require('./helpers/logger');
 const db = require('./providers/db');
 const cors = require('./middlewares/cors');
+
 // Express APP config
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 // custom middlewares
 app.use(cors, reqID, reqTime, requestLogger);
