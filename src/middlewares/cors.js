c3f9bf9bd90b555211b0c1be5ce11158376c5a0a
poizxc <poizxc@users.noreply.github.com>
2020-07-26 19:21:24 +0200

    cors Allow PATCH

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -2,7 +2,7 @@ const { isDev } = require('../config/config');
 
 module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://poizxc.github.io');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept',
